a255cd997bf1a7718ac4e9a6c5367c5017f4dca9
Chaudhary <ChaudhSi@emea.travelex.net>
2018-12-29 14:57:07 +0530

    Changing Timesheet to an array type

--- pc-business/viewmodelhelpers/employeeviewmodelhelper.js
+++ pc-business/viewmodelhelpers/employeeviewmodelhelper.js
@@ -1,14 +1,44 @@
 const EmployeeHelper = require("../modelhelpers/employeehelper").EmployeeHelper;
 const DesignationHelper = require("../modelhelpers/designationhelper").DesignationHelper;
 const TimesheetHelper = require("../modelhelpers/timesheethelper").TimesheetHelper;
-module.exports.EmployeeViewModel = class EmployeeViewModel {
+const EmployeeViewModel = require("../viewmodels/employeeviewmodel").EmployeeViewModel;
+
+module.exports.EmployeeViewModelHelper = class EmployeeViewModelHelper {
     constructor() {
         this.employeeHelper = new EmployeeHelper();
         this.designationHelper = new DesignationHelper();
-        this.timesheetHelper = new EmployeeHelper();
+        this.timesheetHelper = new TimesheetHelper();
     }
 
-    createViewModel(employeeId) {
-        
+    createViewModel(employeeId, callback) {
+        this.employeeHelper.read(employeeId, (employeeReadErr, employeeData) => {
+            if(typeof employeeReadErr != "undefined" && employeeReadErr != null) {
+                callback(employeeReadErr, null);
+            }
+            else {
+                var employeeViewModel = new EmployeeViewModel();
+                employeeViewModel.employee = employeeData;
+                this.designationHelper.read(employeeViewModel.employee.designationId, (designationReadErr, designationData) => {
+                    if(typeof designationReadErr != "undefined" && designationReadErr != null) {
+                        callback(designationReadErr, null);
+                    }
+                    else {
+                        employeeViewModel.designation = designationData;
+    
+                        this.timesheetHelper.read(null, (timesheetReadError, timesheetData) => {
+                            if(typeof timesheetReadError != "undefined" && timesheetReadError != null) {
+                                callback(timesheetReadError, null);
+                            }
+                            else {
+                                var employeeTimesheetData = timesheetData.filter(x => x.employeeId == employeeViewModel.employee.id);
+                                employeeViewModel.timesheet = employeeTimesheetData;
+
+                                callback(null, employeeViewModel);
+                            }
+                        })
+                    }
+                });
+            }
+        })
     }
 }
--- pc-business/viewmodels/employeeviewmodel.js
+++ pc-business/viewmodels/employeeviewmodel.js
@@ -1,3 +1,11 @@
+const EmployeeModel = require("../models/employeemodel").EmployeeModel;
+const DesignationModel = require("../models/designationmodel").DesignationModel;
+const TimesheetModel = require("../models/timesheetmodel").TimsheetModel;
+
 module.exports.EmployeeViewModel = class EmployeeViewModel {
-    
+    constructor() {
+        this.employee = new EmployeeModel();
+        this.designation = new DesignationModel();
+        this.timesheet = [];
+    }
 }
