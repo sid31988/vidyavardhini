f7352b96715dca0d4bff8657e2e861b8abc1c46d
Chaudhary <ChaudhSi@emea.travelex.net>
2018-12-30 13:36:45 +0530

    Trying out the mongodb library

--- pc-server/node_modules/pc-api/node_modules/pc-business/node_modules/pc-data/index.test.js
+++ pc-server/node_modules/pc-api/node_modules/pc-business/node_modules/pc-data/index.test.js
@@ -1,6 +1,40 @@
 const DbContextFactory = require("./src/dbcontextfactory").DbContextFactory;
 const ContextTypeEnum = require("./src/dbcontextfactory").ContextTypeEnum;
 
+const MongoClient = require("mongodb").MongoClient;
+const mongoUrl = "mongodb://localhost:27017";
+
+MongoClient.connect(mongoUrl, { useNewUrlParser: true }, function (err, client) {
+    if(typeof err != "undefined" && err != null) {
+        console.log(`Error found while connecting to '${mongoUrl}'`, err);
+    }
+    else {
+        const timesheetCollection = client.db("vidyavardhini").collection("timesheet");
+        timesheetCollection.find({ $and: [
+            { "employeeId": "1" },
+            { "date": "12-03-2018" }
+        ] }).toArray((err, timesheetData) => {
+            if(typeof err != "undefined" && err != null) {
+                console.log(`Error while reading from collection timesheet`, err);
+            }
+            else {
+                console.log(`Timesheet data found: `, timesheetData);
+
+                timesheetCollection.updateOne({ "employeeId": timesheetData[0].employeeId, "date": timesheetData[0].date }, { $set: { "hours": 10 } }, { upsert: true }, (err, result) => {
+                    if(typeof err != "undefined" && err != null) {
+                        console.log(`Error while reading from collection timesheet`, err);
+                    }
+                    else {
+                        console.log(`Timesheet data updated`, result);
+                    }
+                    client.close();
+                });
+            }
+        });
+    }
+});
+
+/*
 var factory = new DbContextFactory();
 var fileDbContext = factory.createContext(ContextTypeEnum.FileDbContext, "user");
 var userItem = {
@@ -36,5 +70,4 @@ fileDbContext.insert(userItem, (err, result) => {
         });
     }
 });
-
-
+*/
